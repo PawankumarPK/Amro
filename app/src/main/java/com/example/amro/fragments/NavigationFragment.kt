eb90f25852ba2a45fb21a3c72abe21771458a922
PawankumarPK <pk9910765616@gmail.com>
2019-09-30 23:24:00 +0530

    Add map in fragment

--- app/src/main/java/com/example/amro/fragments/NavigationFragment.kt
+++ app/src/main/java/com/example/amro/fragments/NavigationFragment.kt
@@ -1,12 +1,13 @@
 package com.example.amro.fragments
 
-import com.example.amro.api.LocationStats
-import com.example.amro.api.RetrofitClient
-import com.example.amro.api.TripDetails
-import com.example.amro.api.models.Navigation.RosMap
-import com.example.amro.api.models.StandardModels.StdStatusModel
-import android.graphics.*
+
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.PointF
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import com.example.amro.utils.Helper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -14,6 +15,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import com.example.amro.R
+import com.example.amro.api.LocationStats
+import com.example.amro.api.RetrofitClient
+import com.example.amro.api.TripDetails
+import com.example.amro.api.models.StandardModels.StdStatusModel
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.fragment_navigation.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,114 +33,64 @@ class NavigationFragment : BaseFragment(), View.OnTouchListener {
 
 
     var sendingGoal_inProgress = false
+    val TAG = "NavFrag"
 
-
-    private val TAG = "Touch"
-    private val MIN_ZOOM = 1f
-    private val MAX_ZOOM = 1f
-
-    // These matrices will be used to scale points of the image
     var matrix = Matrix()
     var savedMatrix = Matrix()
 
-    // The 3 states (events) which the user is trying to perform
     val NONE = 0
     val DRAG = 1
     val ZOOM = 2
     var mode = NONE
 
-    // these PointF objects are used to record the point(s) the user is touching
     var start = PointF()
     var mid = PointF()
     var oldDist = 1f
 
+    var control_mode = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(com.example.amro.R.layout.fragment_navigation, container, false)
-
-        view.setOnTouchListener(View.OnTouchListener { v, event ->
-
-            val x = event.x
-            val y = event.y
-            if (!sendingGoal_inProgress) SendGoal(x, y)
-
-            Log.i("----->NavFrag", "$x $y")
-
-            true
-        })
-        return view
+        return inflater.inflate(R.layout.fragment_navigation, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mTestZoom.setOnTouchListener(this)
+        LoadMap()
+        updateBotXY()
     }
 
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
         super.setUserVisibleHint(isVisibleToUser)
         if (isVisibleToUser) {
-            LoadMap()
-            updateBotXY()
+        }
+    }
+
+
+    var target: Target = object : Target {
+        override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+            if (bitmap != null) {
+                mTestZoom.setImageBitmap(bitmap)
+            }
+            Handler().postDelayed({
+                LoadMap()
+            }, 2000)
+        }
+
+        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+        }
+
+        override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            Handler().postDelayed({
+                LoadMap()
+            }, 5000)
         }
     }
 
     fun LoadMap() {
-        val api = RetrofitClient.rosService
-        val call = api.getMap()
-
-        call.enqueue(object : Callback<RosMap> {
-            override fun onFailure(call: Call<RosMap>?, t: Throwable?) {
-                Toast.makeText(baseActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
-
-            }
-
-            override fun onResponse(call: Call<RosMap>, response: Response<RosMap>) {
-                Toast.makeText(baseActivity, "Got Map", Toast.LENGTH_SHORT).show()
-                Log.i("----->Map:", response.body().width.toString())
-                updateMapUI(response.body()!!)
-            }
-        })
-    }
-
-
-    fun updateMapUI(mapData: RosMap) {
-        infoMessage.text = ""
-
-        var rows: Int = mapData.height!!.toInt()
-        var cols: Int = mapData.width!!.toInt()
-
-        var v: View = object : View(baseActivity!!.applicationContext) {
-            override fun onDraw(canvas: Canvas?) {
-                if (canvas != null) {
-                    val paint = Paint()
-                    paint.style = Paint.Style.FILL
-                    var dVal = 0
-                    var index = 0
-                    for (j in 0..rows) {
-                        for (i in 0..cols) {
-                            index = i + (j * cols)
-                            dVal = -1
-                            if (index > 0 && index < mapData.data!!.size) dVal = mapData.data!![index]
-
-                            when (dVal) {
-                                -1 -> paint.color = Color.GRAY
-                                0 -> paint.color = Color.BLACK
-                                else -> paint.color = Color.parseColor("#0096CB")
-                            }
-
-                            canvas.drawPoint(i.toFloat(), j.toFloat(), paint)
-                            canvas.save()
-                        }
-                    }
-                }
-                super.onDraw(canvas)
-            }
-        }
-        v.layout(0, 0, cols, rows)
-        canvasHolder.x = 0F
-        canvasHolder.y = 0F
-
-        canvasHolder.addView(v)
+        Picasso.get().load(Helper.getConfigValue(baseActivity!!,"ros_url")!! + "/map").memoryPolicy(MemoryPolicy.NO_CACHE).into(target)
+        Log.i(TAG, "Loading Image...")
     }
 
     fun SendGoal(x: Float, y: Float) {
@@ -177,76 +136,71 @@ class NavigationFragment : BaseFragment(), View.OnTouchListener {
         }.start()
     }
 
-
     override fun onTouch(v: View, event: MotionEvent): Boolean {
-        val view = v as ImageView
-        view.scaleType = ImageView.ScaleType.MATRIX
-        val scale: Float
+        //dumpEvent(event)
 
-        dumpEvent(event)
-        // Handle touch events here...
+        if (control_mode == 1) {
+            val x = event.x
+            val y = event.y
+            if (!sendingGoal_inProgress) SendGoal(x, y)
+            Log.i(TAG, "$x $y")
+        } else {
+            val view = v as ImageView
+            view.scaleType = ImageView.ScaleType.MATRIX
+            val scale: Float
 
-        when (event.action and MotionEvent.ACTION_MASK) {
 
-            // first finger down only
-            MotionEvent.ACTION_DOWN -> {
-                matrix.set(view.imageMatrix)
-                savedMatrix.set(matrix)
-                start.set(event.x, event.y)
-                Log.d(TAG, "mode=DRAG") // write to LogCat
-                mode = DRAG
-            }
+            when (event.action and MotionEvent.ACTION_MASK) {
 
-            // first finger lifted
-            MotionEvent.ACTION_UP,
-
-            MotionEvent.ACTION_POINTER_UP -> {
-                // second finger lifted
-                mode = NONE
-                Log.d(TAG, "mode=NONE")
-            }
-
-            MotionEvent.ACTION_POINTER_DOWN -> {
-                // first and second finger down
-                oldDist = spacing(event)
-                Log.d(TAG, "oldDist=$oldDist")
-                if (oldDist > 5f) {
+                MotionEvent.ACTION_DOWN -> {
+                    matrix.set(view.imageMatrix)
                     savedMatrix.set(matrix)
-                    midPoint(mid, event)
-                    mode = ZOOM
-                    Log.d(TAG, "mode=ZOOM")
+                    start.set(event.x, event.y)
+                    Log.d(TAG, "mode=DRAG") // write to LogCat
+                    mode = DRAG
                 }
-            }
 
-            MotionEvent.ACTION_MOVE ->
-                if (mode == DRAG) {
-                    matrix.set(savedMatrix)
-                    matrix.postTranslate(event.x - start.x, event.y - start.y) // create the transformation in the matrix  of points
-                } else if (mode == ZOOM) {
-                    // pinch zooming
-                    val newDist = spacing(event)
-                    Log.d(TAG, "newDist=$newDist")
-                    if (newDist > 5f) {
-                        matrix.set(savedMatrix)
-                        scale = newDist / oldDist // setting the scaling of the
-                        // matrix...if scale > 1 means
-                        // zoom in...if scale < 1 means
-                        // zoom out
-                        matrix.postScale(scale, scale, mid.x, mid.y)
+                MotionEvent.ACTION_UP,
+
+                MotionEvent.ACTION_POINTER_UP -> {
+                    mode = NONE
+                    Log.d(TAG, "mode=NONE")
+                }
+
+                MotionEvent.ACTION_POINTER_DOWN -> {
+                    oldDist = spacing(event)
+                    Log.d(TAG, "oldDist=$oldDist")
+                    if (oldDist > 5f) {
+                        savedMatrix.set(matrix)
+                        midPoint(mid, event)
+                        mode = ZOOM
+                        Log.d(TAG, "mode=ZOOM")
                     }
                 }
+
+                MotionEvent.ACTION_MOVE ->
+                    if (mode == DRAG) {
+                        matrix.set(savedMatrix)
+                        matrix.postTranslate(event.x - start.x, event.y - start.y) // create the transformation in the matrix  of points
+                    } else if (mode == ZOOM) {
+                        // pinch zooming
+                        val newDist = spacing(event)
+                        Log.d(TAG, "newDist=$newDist")
+                        if (newDist > 5f) {
+                            matrix.set(savedMatrix)
+                            scale = newDist / oldDist // setting the scaling of the
+                            // matrix...if scale > 1 means
+                            // zoom in...if scale < 1 means
+                            // zoom out
+                            matrix.postScale(scale, scale, mid.x, mid.y)
+                        }
+                    }
+            }
+            view.imageMatrix = matrix
         }
 
-        view.imageMatrix = matrix // display the transformation on screen
-        return true // indicate event was handled
+        return true
     }
-
-    /*
-     * --------------------------------------------------------------------------
-     * Method: spacing Parameters: MotionEvent Returns: float Description:
-     * checks the spacing between the two fingers on touch
-     * ----------------------------------------------------
-     */
 
     private fun spacing(event: MotionEvent): Float {
         val x = event.getX(0) - event.getX(1)
@@ -254,20 +208,12 @@ class NavigationFragment : BaseFragment(), View.OnTouchListener {
         return Math.sqrt((x * x + y * y).toDouble()).toFloat()
     }
 
-    /*
-     * --------------------------------------------------------------------------
-     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
-     * Description: calculates the midpoint between the two fingers
-     * ------------------------------------------------------------
-     */
-
     private fun midPoint(point: PointF, event: MotionEvent) {
         val x = event.getX(0) + event.getX(1)
         val y = event.getY(0) + event.getY(1)
         point.set(x / 2, y / 2)
     }
 
-    /** Show an event in the LogCat view, for debugging  */
     private fun dumpEvent(event: MotionEvent) {
         val names = arrayOf("DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE", "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?")
         val sb = StringBuilder()
